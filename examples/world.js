83e1181e91e56aab4268f182b7ac61c3192ede76
Tai Zeming <tai.zeming@pithos.net>
2025-12-24 17:50:50 +0800

    chore: update example

--- examples/world.js
+++ examples/world.js
@@ -7,24 +7,46 @@ const height = canvas.clientHeight * pixelRatio;
 
 const world = new World({ canvas, width, height });
 
-// const sprite = createSprite(world);
+{
+	const sprite = createSprite(world);
 
-// sprite.transform.translate = { x: width / 2, y: height / 2 };
-// sprite.transform.scale = { x: 50, y: 50 };
-// sprite.transform.rotation = Math.PI / 4;
+	sprite.transform.translate = { x: width / 2, y: height / 2 };
+	sprite.transform.scale = { x: 50, y: 50 };
+	sprite.transform.rotation = Math.PI / 4;
 
-// sprite.region.position = { x: -0.5, y: -0.5 };
-// sprite.region.size = { width: 1, height: 1 };
+	sprite.region.position = { x: -0.5, y: -0.5 };
+	sprite.region.size = { width: 0.3, height: 0.3 };
+}
+
+{
+	const sprite = createSprite(world);
+
+	sprite.transform.translate = { x: width / 2, y: height / 2 };
+	sprite.transform.scale = { x: 50, y: 50 };
+	sprite.transform.rotation = Math.PI / 4;
+
+	sprite.region.position = { x: -0.1, y: -0.2 };
+	sprite.region.size = { width: 0.3, height: 0.4 };
+}
 
 const random = () => {
 	return (Math.random() - 0.5) * 2;
 };
 
-for (let i = 0; i < 1000; i++) {
+{
 	const sprite = createSprite(world);
+	sprite.transform.translate = { x: width / 2 + 100, y: height / 2 + 100 };
+	sprite.transform.scale = { x: 80, y: 80 };
+	sprite.transform.rotation = Math.PI / 6;
+
 	sprite.region.position = { x: random(), y: random() };
-	sprite.region.size = { width: Math.random(), height: Math.random() };
+	sprite.region.size = { width: 0.2, height: 0.2 };
 }
 
+// for (let i = 0; i < 1000; i++) {
+// 	const sprite = createSprite(world);
+// 	sprite.region.position = { x: random(), y: random() };
+// 	sprite.region.size = { width: Math.random(), height: Math.random() };
+// }
 
 world.run();
